8f3f6050b043855fce909b842cc9c0f89cd0ee17
ankithsavio <ankithsavio@gmail.com>
2024-06-25 20:42:41 +0530

    update logs

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ import logging
 import torch
 import math
 
-logging.basicConfig(filename='logs/logfile.log' ,level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 client = arxiv.Client()
 
