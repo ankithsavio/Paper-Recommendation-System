a9d568767c6512ec6efd35359309e127f1e82908
ankithsavio <ankithsavio@gmail.com>
2024-06-25 20:32:32 +0530

    add logs

--- script.py
+++ script.py
@@ -7,9 +7,11 @@ import arxiv
 from transformers import AutoTokenizer, AutoModel
 from pinecone import Pinecone
 from config import PINECONE_API_KEY, INDEX_NAME, NAMESPACE_NAME, CHECKPOINT_PATH, ARXIV_CATEGORY_NAME, ARXIV_COMMENT_QUERY
-
+import logging
 import torch
 import math
+
+logging.basicConfig(filename='logs/logfile.log' ,level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 client = arxiv.Client()
 
@@ -23,51 +25,65 @@ search = arxiv.Search(
     sort_by= arxiv.SortCriterion.SubmittedDate
 )
 
+results = list(client.results(search))
+if not results:
+    logging.warning("No results found for the given query. Check the README file for the necessary info.")
+    exit()
+
 df = pd.DataFrame({'Title': [result.title for result in client.results(search)],
               'Abstract': [result.summary.replace('\n', ' ') for result in client.results(search)],
               'Date': [result.published.date().strftime('%Y-%m-%d') for result in client.results(search)],
               'id': [result.entry_id for result in client.results(search)]})
 
-df_main = pd.read_csv('arxiv-scrape.csv')
+try:
+    df_main = pd.read_csv('arxiv-scrape.csv')
+except FileNotFoundError:
+    logging.warning("CSV file not found, Project isnt initialized.")
+    exit()
+
 df.reset_index(inplace=True)
 df.drop(columns=['index'], inplace=True)
 union_df = df.merge(df_main, how='left', indicator=True)
 df = union_df[union_df['_merge'] == 'left_only'].drop(columns=['_merge'])
 
-df_main = pd.concat([df_main, df], ignore_index= True)
-df_main.drop_duplicates(inplace= True)
-df_main.to_csv('arxiv-scrape.csv', index = False)
-
-tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT_PATH)
-model = AutoModel.from_pretrained(CHECKPOINT_PATH)
-
-title_abs = [title + tokenizer.sep_token + abstract for title,abstract in zip(df['Title'], df['Abstract'])]
-
-embeddings = torch.empty((0, 768))
-batch_size = 15
-for i in range(math.ceil(len(title_abs)/batch_size)):
-    inputs = tokenizer(title_abs[i * batch_size:(i + 1) * batch_size], padding=True, truncation=True, return_tensors="pt", max_length=512)
-    result = model(**inputs)
-    embeddings = torch.cat((embeddings, result.last_hidden_state[:, 0, :]), dim = 0)
-
-pc = Pinecone(api_key = PINECONE_API_KEY)
-if INDEX_NAME in pc.list_indexes().names():
-    index = pc.Index(INDEX_NAME)
+if df.empty:
+    logging.info("No new papers found.")
 else:
-    print(f"{INDEX_NAME} doesnt exist. Run init-script first.")
-    exit()
+    df_main = pd.concat([df_main, df], ignore_index= True)
+    df_main.drop_duplicates(inplace= True)
+    df_main.to_csv('arxiv-scrape.csv', index = False)
 
-results = []
-score_threshold = 2.61
-for i,embedding in enumerate(embeddings):
-    query = embedding.detach().numpy().tolist()
-    result = index.query(namespace=NAMESPACE_NAME,vector=query,top_k=3,include_values=False)
-    sum_score = 0
-    for match in result['matches']:
-        sum_score += match['score'] 
-    if sum_score > score_threshold:
-        results.append({'id':df['id'][i], 'total_score': sum_score})
+    tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT_PATH)
+    model = AutoModel.from_pretrained(CHECKPOINT_PATH)
+
+    title_abs = [title + tokenizer.sep_token + abstract for title,abstract in zip(df['Title'], df['Abstract'])]
+
+    embeddings = torch.empty((0, 768))
+    batch_size = 15
+    for i in range(math.ceil(len(title_abs)/batch_size)):
+        inputs = tokenizer(title_abs[i * batch_size:(i + 1) * batch_size], padding=True, truncation=True, return_tensors="pt", max_length=512)
+        with torch.no_grad():
+            result = model(**inputs)
+        embeddings = torch.cat((embeddings, result.last_hidden_state[:, 0, :]), dim = 0)
+
+    pc = Pinecone(api_key = PINECONE_API_KEY)
+    if INDEX_NAME in pc.list_indexes().names():
+        index = pc.Index(INDEX_NAME)
+    else:
+        logging.error(f"{INDEX_NAME} doesnt exist. Run init-script first.")
+        exit()
+
+    results = []
+    score_threshold = 2.61
+    for i,embedding in enumerate(embeddings):
+        query = embedding.detach().numpy().tolist()
+        result = index.query(namespace=NAMESPACE_NAME,vector=query,top_k=3,include_values=False)
+        sum_score = sum(match['score'] for match in result['matches'])
+        if sum_score > score_threshold:
+            results.append({'id':df['id'][i], 'total_score': sum_score})
 
 
-for result in results:
-    print(f"Possible interesting paper : {result['id']} \nwith Average Score = {result['total_score']/3}")
+    for result in results:
+        logging.info(f"Possible interesting paper : {result['id']} \nwith Average Score = {result['total_score']/3}\n")
+
+logging.info("Weekly Paper Recommendation Script Completed.")
